1a02ab07752aff7dbd64a12ba68c4196dc419e08
Tyler Jacobson <TylerIRL2@gmail.com>
2022-02-13 20:12:32 -0800

    All basic functionality complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Select from 'react-select';
 import './App.css';
+import popularBuilds from "./popular_builds";
+import engravingsList from "./engravings_list";
+
+
+
+function nameToEngravingObject(name) {
+  return { label: name, value: name }
+}
+
+const engravings = []
+
+engravingsList.forEach(engraingName => {
+  engravings.push(nameToEngravingObject(engraingName))
+})
+
+const defaultSelectedOptions = { formOne: null, formTwo: null }
 
 function App() {
+
+  const [selectedOptions, setSelectedOptions] = useState(defaultSelectedOptions)
+  const [perfectMatchList, setPerfectMatchList] = useState([])
+  const [partialMatchList, setPartialMatchList] = useState([])
+
+  const onChange = (incomingSelected, e) => {
+    setSelectedOptions({
+      ...selectedOptions,
+      [e.name]: incomingSelected
+    })
+  }
+
+  const onSubmit = () => {
+
+    console.log(selectedOptions);
+    console.log(popularBuilds);
+    const perfectMatchList = [];
+    const partialMatchList = [];
+
+    popularBuilds.map((build) => {
+      if (build.build_engravings.includes(selectedOptions.formOne.value) && build.build_engravings.includes(selectedOptions.formTwo.value)) {
+        return perfectMatchList.push(build)
+      } else if (build.build_engravings.includes(selectedOptions.formOne.value) || build.build_engravings.includes(selectedOptions.formTwo.value)) {
+        return partialMatchList.push(build)
+      }
+    })
+    setPerfectMatchList(perfectMatchList)
+    setPartialMatchList(partialMatchList)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+        <Select
+          options={engravings} onChange={(inc, e) => onChange(inc, e)} name="formOne"
+        />
+        <Select
+          options={engravings} onChange={onChange} name="formTwo"
+        />
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+      <div>
+        <h2>Perfect Matches</h2>
+        {
+          perfectMatchList.map((build) => {
+            return <h3>Both Engravings Used In: <span className="perfect-match">{build.build_name}</span></h3>
+          })
+        }
+        <h2>Partial Matches</h2>
+        {
+          partialMatchList.map((build) => {
+            return <h3>One Engraving Used In: <span className="partial-match">{build.build_name}</span></h3>
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/popular_builds.js
@@ -0,0 +1,59 @@
+const popular_builds = [
+    {
+        build_name: "Mayhem Berserker",
+        build_engravings: ["Mayhem", "Master's Tenacity", "Raid Captain", "Keen Blunt Weapon", "Grudge", "Cursed Doll"]
+    },
+    {
+        build_name: "Berserker's Technique",
+        build_engravings: ["Berserker's Technique", "Raid Captain", "Keen Blunt Weapon", "Grudge", "Cursed Doll", "Stabilized Status"]
+    },
+    {
+        build_name: "Blue Warlord Gunlancer",
+        build_engravings: ["Combat Readiness", "Barricade", "Cursed Doll", "Grudge", "Stabilized Status", "Adrenaline"]
+    },
+    {
+        build_name: "Red Warlord Gunlancer",
+        build_engravings: ["Lone Knight", "Combat Readiness", "Grudge", "Super Charge",  "Cursed Doll", "Master Brawler"]
+    },
+    {
+        build_name: "Judgement Paladin",
+        build_engravings: ["Keen Blunt Weapon", "Cursed Doll", "Judgment", "Stabilized Status", "Grudge", "Adrenaline"]
+    },
+    {
+        build_name: "Aura of Blessing Paladin",
+        build_engravings: ["Awakening", "Drops of Ether", "Vital Point Hit", "Expert", "Blessed Aura", "Judgment"]
+    },
+    {
+        build_name: "Deathblow Striker",
+        build_engravings: ["Master of Ambush", "Keen Blunt Weapon", "Grudge", "Deathblow", "Cursed Doll", "Adrenaline"]
+    },
+    {
+        build_name: "Esoteric Flurry Striker",
+        build_engravings: ["Esoteric Flurry", "Raid Captain", "Keen Blunt Weapon", "Grudge", "Cursed Doll", "Adrenaline"]
+    },
+    {
+        build_name: "Robust Spirit Soulfist",
+        build_engravings: ["Master of Ambush", "Keen Blunt Weapon", "Grudge", "Deathblow", "Cursed Doll", "Adrenaline"]
+    },
+
+
+
+
+    {
+        build_name: "Energy Overflow Soulfist",
+        build_engravings: ["Esoteric Flurry", "Raid Captain", "Keen Blunt Weapon", "Grudge", "Cursed Doll", "Adrenaline"]
+    },
+
+
+
+
+    {
+        build_name: "Peacemaker Gunslinger",
+        build_engravings: ["Peacemaker", "Grudge", "Others"]
+     },
+     {
+        build_name: "On The Hunt Gunslinger",
+        build_engravings: ["On The Hunt", "Peacemaker", "Grudge", "Assault Captain"]
+     }
+    ]
+export default popular_builds;
