c621bc7602dcd8287792333d90fe3fb139949524
eko-develops <ekodevelops@gmail.com>
2022-02-22 10:35:39 -0800

    Remove spans between nav links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -18,9 +18,7 @@ function Nav() {
             </div>
             <div className={darkModeRedux ? "nav-links-darkmode" : "nav-links"}>
                 <Link className="nav-link" to="/">Engraving Search</Link>
-                <span className="nav-link-span"/>
                 <Link className="nav-link" to="/top-engravings">Top Engravings</Link>
-                <span className="nav-link-span"/>
                 <Link className="nav-link" to="/builds-list">Builds</Link>
             </div>
             <a className="discord-shilling" href="https://discord.gg/x82dcfNcjw" alt="Discord link" target="_blank" rel="noreferrer">Join my <span className="discord-link-light">Discord</span> if you have feedback or suggestions. If you suggest something it will probably be added</a>
